938cb4a85b0b4bafc450c5e4969906bf6f4e1b6e
AlexanderHaan <haan.alex@outlook.de>
2020-06-26 17:01:53 +0200

    First Test is working

--- src/Test/DatenmodellTests.java
+++ src/Test/DatenmodellTests.java
@@ -2,9 +2,8 @@
 package Test;
 
 import Application.Datenmodell;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
@@ -21,34 +20,28 @@ public class DatenmodellTests {
 		dataModel.readDataFromFile("movieproject2020.db");
 	}
 
-	@Before
-	public void setUpStreams() {
-		System.setOut(new PrintStream(outContent));
-		System.setErr(new PrintStream(errContent));
-	}
-
-	@After
-	public void restoreStreams() {
-		System.setOut(originalOut);
-		System.setErr(originalErr);
-	}
 
 	@Test
 	public void testSearchPerson() {
+		System.setOut(new PrintStream(outContent));
+		System.setErr(new PrintStream(errContent));
 		dataModel.searchPerson("Roger");
 		String expectedOutput = 
-			"ID:                              53\n"+
-			"Name:                            Jaime Rogers\n"+
-			"Geschlecht:                      Male\n"+
-			"Anzahl der Freunde:              4\n"+
-			"Anzahl der gekauften Produkte:   1\n"+
-			"\n"+
-			"ID:                              70\n"+
-			"Name:                            Roger Walker\n"+
-			"Geschlecht:                      Male\n"+
-			"Anzahl der Freunde:              6\n"+
-			"Anzahl der gekauften Produkte:   1\n";
-		assertEquals(expectedOutput, outContent.toString());
+			"ID:                              53\r\n"+
+			"Name:                            Jaime Rogers\r\n"+
+			"Geschlecht:                      Male\r\n"+
+			"Anzahl der Freunde:              4\r\n"+
+			"Anzahl der gekauften Produkte:   1\r\n"+
+			"\r\n"+
+			"ID:                              70\r\n"+
+			"Name:                            Roger Walker\r\n"+
+			"Geschlecht:                      Male\r\n"+
+			"Anzahl der Freunde:              6\r\n"+
+			"Anzahl der gekauften Produkte:   1\r\n"+
+			"\r\n";
+		Assertions.assertEquals(expectedOutput, outContent.toString());
+		System.setOut(originalOut);
+		System.setErr(originalErr);
 	}
 }
 //*/
